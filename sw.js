b1a7c3948deda3a8a813de1a9c6dec1f906c43f6
movveplaneta <ltrolandy@gmail.com>
2026-02-08 21:16:48 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10587220
+    "zoneId": 10587237
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
